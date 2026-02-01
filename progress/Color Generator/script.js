7089b4a0f8685cf1523c1dfa1d977780dca1d11a
unknown <ahmadalqrni2002@gmail.com>
2026-02-01 22:06:00 +0300

    fix

--- progress/Color Generator/script.js
+++ progress/Color Generator/script.js
@@ -1,15 +1,12 @@
+colorbutton = document.getElementById("color-button-generator");
 
-
-colorbutton = document.getElementById("color-button-generator")
-
-function randomcolor(){
-    const red = Math.floor(Math.random()*256);
-    const blue = Math.floor(Math.random()*256);
-    const green = Math.floor(Math.random()*256);
-    return `rgb(${red},${blue},${green})`; 
+function randomcolor() {
+  const red = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  return `rgb(${red},${blue},${green})`;
 }
 
 colorbutton.addEventListener("click", () => {
-    document.body.style.backgroundColor = randomcolor();
-})
-return document.body.style.backgroundColor = randomcolor();
+  document.body.style.backgroundColor = randomcolor();
+});
