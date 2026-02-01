8791bea712d1a89b3d5a04de78c4e9e9a5356f98
lazy_owl <ahmadalqrni2002@gmail.com>
2026-02-01 06:08:10 +0300

    test

--- progress/randomtest/randomtes.js
+++ progress/randomtest/randomtes.js
@@ -1,53 +1,6 @@
-const buttonEl = document.getElementById("roll-button");
 
-const diceEl = document.getElementById("dice");
-
-const rollHistoryEl = document.getElementById("roll-history");
-
-let historyList = [];
-
-function rollDice() {
-  const rollResult = Math.floor(Math.random() * 6) + 1;
-  const diceFace = getDiceFace(rollResult);
-  diceEl.innerHTML = diceFace;
-  historyList.push(rollResult);
-  updateRollHistory();
-}
-
-function updateRollHistory() {
-  rollHistoryEl.innerHTML = "";
-  for (let i = 0; i < historyList.length; i++) {
-    const listItem = document.createElement("li");
-    listItem.innerHTML = `Roll ${i + 1}: <span>${getDiceFace(
-      historyList[i],
-    )}</span>`;
-    rollHistoryEl.appendChild(listItem);
+    const x = 121;
+  const str = x.toString();  // Convert to string: '121'
+  for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);  // Prints '1', '2', '1'
   }
-}
-
-function getDiceFace(rollResult) {
-  switch (rollResult) {
-    case 1:
-      return "&#9856;";
-    case 2:
-      return "&#9857;";
-    case 3:
-      return "&#9858;";
-    case 4:
-      return "&#9859;";
-    case 5:
-      return "&#9860;";
-    case 6:
-      return "&#9861;";
-    default:
-      return "";
-  }
-}
-
-buttonEl.addEventListener("click", () => {
-  diceEl.classList.add("roll-animation");
-  setTimeout(() => {
-    diceEl.classList.remove("roll-animation");
-    rollDice();
-  }, 1000);
-});
