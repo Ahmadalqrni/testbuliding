a096bcc6149917c83e57e6f1eae62d448e477148
lazy_owl <ahmadalqrni2002@gmail.com>
2026-02-01 16:38:10 +0300

    Color Generator page

---
+++ progress/Color Generator/script.js
@@ -0,0 +1,15 @@
+
+
+colorbutton = document.getElementById("color-button-generator")
+
+function randomcolor(){
+    const red = Math.floor(Math.random()*256);
+    const blue = Math.floor(Math.random()*256);
+    const green = Math.floor(Math.random()*256);
+    return `rgb(${red},${blue},${green})`; 
+}
+
+colorbutton.addEventListener("click", () => {
+    document.body.style.backgroundColor = randomcolor();
+})
+return document.body.style.backgroundColor = randomcolor();
--- progress/randomtest/randomtes.js
+++
@@ -1,6 +0,0 @@
-
-    const x = 121;
-  const str = x.toString();  // Convert to string: '121'
-  for (let i = 0; i < str.length; i++) {
-    console.log(str[i]);  // Prints '1', '2', '1'
-  }
